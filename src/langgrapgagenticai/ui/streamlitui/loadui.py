a783263b455ff5c40ab2ed8eb3f22d8af5416d8f
shubhashish1 <shubhashish.mishra20@gmail.com>
2025-10-28 15:53:05 +0530

    Creating loadui file to load the ui

--- src/langgrapgagenticai/ui/streamlitui/loadui.py
+++ src/langgrapgagenticai/ui/streamlitui/loadui.py
@@ -19,3 +19,28 @@ class LoadStreamlitUI:
         self.config = Config()
         # Now let's load the user controls that we are getting from uiconfigfile.ini file
         self.user_controls = {}
+
+    # Now let's write code to load the streamlit ui
+
+    def load_streamlit_ui(self):
+        st.set_page_config(page_title= self.config.get_page_title(), layout="wide")
+        st.header(self.config.get_page_title())
+        st.session_state.timeframe = ''
+        st.session_state.IsFetchButtonClicked = False # TO track whether the button is clickedd on not
+        st.session_state.IsSDLC = False
+
+        # Now let's load the sidebar 
+
+        with st.sidebar:
+            # Get options from config
+            llm_options = self.config.get_llm_option()
+            usecase_options = self.config.get_usecase_options()
+
+            # LLM selection
+            self.user_controls["selected_llm"] = st.selectbox("Select LLM", llm_options) # Here used the use controls to provide option for user to select the llm they want
+
+            if self.user_controls["selected_llm"] == "Groq":
+                # model selection
+                model_options = self.config.get_groq_model_options()
+                self.user_controls["selected_groq_model"] = st.selectbox("Select Model", model_options)
+                # Here we have created a dropbox or selectbox of usercontrol types
